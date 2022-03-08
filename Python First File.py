bec0d18dd60b10686c83bb9ba85a771a675374e1
Joseph <jfscyber9@gmail.com>
2022-03-08 15:00:08 -0500

    first commit

---
+++ Python First File.py
@@ -0,0 +1,14 @@
+#Joseph Suter ---> Edit ---> 07-04-2020
+import random
+import webbrowser as wb
+
+print("Hello, I am the first program written on the MAC");
+print("My name is Obu");
+
+password = ("AnimeBot")
+password2 = ("Guest")
+entry = input("please input the password or Guest if you don't know: ");
+if entry == password:
+    print("You have input the password welcome!")
+
+    
